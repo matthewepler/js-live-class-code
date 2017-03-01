2fe426d83994fb2be10be85d11e9317ee9634f32
Matthew Epler <matthewepler@gmail.com>
2017-03-01 08:59:11 -0500

    adds simple todo list for review via PR

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
-* Put your JavaScript here
-*/
+let state = {
+  todos: [
+    { title: 'First todo', complete: false },
+    { title: 'Second todo', complete: true },
+    { title: 'Third todo', complete: true },
+  ],
+};
 
-console.log( 'Open the dev console to see me. :-)' );
+document.onload = init();
+
+function init() {
+	const listNode = document.getElementById('list');
+
+	const filterButton = document.getElementById('filter-button')
+	filterButton.addEventListener('click', (event) => {
+		const items = document.querySelectorAll('#list li.todo--complete');
+		items.forEach( item => item.classList.toggle('hide') );
+	})
+
+	state.todos.forEach( item => addTodoNode(item) )
+
+	function addTodoNode ({title, complete}) {
+		let item = document.createElement('li');
+		item.textContent = title;
+		if (complete === true) item.classList.add('todo--complete');
+		item.addEventListener('click', event => event.target.classList.toggle('todo--complete') );
+		listNode.appendChild(item);
+	};
+}
+
+
 
